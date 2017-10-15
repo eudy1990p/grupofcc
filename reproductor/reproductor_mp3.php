c9b37ee98a981b5d43d1f422e3b24c192f296ef6
Eudy Arias <eudy1990@gmail.com>
2017-10-15 12:42:48 -0400

    reproductor con listado

---
+++ reproductor/reproductor_mp3.php
@@ -0,0 +1,27 @@
+<?php
+$con = mysqli_connect("localhost","root","","db_reproductor");
+    $sql = "select * from mp3 ";
+    $Query = mysqli_query($con,$sql);
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Reproduciendo: </title>
+    </head>
+    <body>
+        <section>
+            <h4><?php echo $_GET["nombreMP3"]; ?></h4>
+            <audio controls autoplay>
+                <source src="mp3/<?php echo $_GET["rutaMP3"]; ?>" type="audio/mpeg"  />
+            </audio>
+        </section>
+        <aside>
+            <h1>Listado de canciones</h1>
+            <ul>
+                <?php while($registro = mysqli_fetch_array($Query)){ ?>
+                <li><a href="?idMP3=<?php echo $registro["id"]; ?>&nombreMP3=<?php echo $registro["nombre"]; ?>&rutaMP3=<?php echo $registro["ruta"]; ?>"><?php echo $registro["nombre"]; ?></a></li>
+                <?php } ?>
+            </ul>
+        </aside>
+    </body>
+</html>
